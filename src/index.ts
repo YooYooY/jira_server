f73239ff74135ef3e5d10b490b8dfd68442cabc2
chenwl <13751840799@163.com>
2021-06-12 16:45:26 +0800

    feat: 链接数据库，启动项目

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,32 @@ import "reflect-metadata";
 import express from "express";
 import cors from "cors";
 
-export default {};
+import createDatabaseConnection from "@/database/createConnection";
+
+const establishDatabaseConnection = async (): Promise<void> => {
+  try {
+    await createDatabaseConnection();
+    console.log("database connect success!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const initializeExpress = (): void => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Serving on http://localhost:${port}`);
+  });
+};
+
+const initializeApp = async (): Promise<void> => {
+  await establishDatabaseConnection();
+  initializeExpress();
+};
+
+initializeApp();
