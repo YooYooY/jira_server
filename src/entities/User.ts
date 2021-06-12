a6da986ad1a663d717669aef7c3d58e0664d3ad7
chenwl <13751840799@163.com>
2021-06-12 16:31:45 +0800

    feat: 创建User实体

---
+++ src/entities/User.ts
@@ -0,0 +1,30 @@
+import is from "@/utils/validation";
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+  static validations = {
+    name: [is.required(), is.maxLength(100)],
+    email: [is.required(), is.email(), is.maxLength(200)]
+  };
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column("varchar")
+  name: string;
+
+  @Column("varchar")
+  email: string;
+
+  @Column("varchar", { length: 2000 })
+  avatarUrl: string;
+
+  @CreateDateColumn({ type: "timestamp" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  updatedAt: Date;
+}
+
+export default User;
